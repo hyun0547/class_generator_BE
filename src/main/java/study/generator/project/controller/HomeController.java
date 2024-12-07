c98eaa1ae559c6510115773567fc1d0a06aa46c1
hyun0547 <hyun0547@gmail.com>
2024-12-07 14:03:57 +0900

    Spring init

---
+++ src/main/java/study/generator/project/controller/HomeController.java
@@ -0,0 +1,13 @@
+package study.generator.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, REST API!";
+    }
+}
