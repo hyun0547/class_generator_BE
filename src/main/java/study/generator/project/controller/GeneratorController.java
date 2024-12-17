1f69940699d6e08d838ffed2e9d57ed34a05b6b6
hyun0547 <hyun0547@gmail.com>
2024-12-17 11:07:08 +0900

    Modify_controller_return type_file

--- src/main/java/study/generator/project/controller/GeneratorController.java
+++ src/main/java/study/generator/project/controller/GeneratorController.java
@@ -1,17 +1,24 @@
 package study.generator.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import study.generator.project.model.GeneratorDTO;
+import study.generator.project.service.GeneratorService;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
 public class GeneratorController {
 
+    @Autowired
+    private GeneratorService generatorService;
+
     @PostMapping("/")
-    public String Generator(@ModelAttribute GeneratorDTO model) {
+    @CrossOrigin(origins = "http://localhost:8081")
+    public ResponseEntity<byte[]> Generator(@ModelAttribute GeneratorDTO model) {
         StringBuilder classBuilder = new StringBuilder();
         classBuilder.append(String.format("public class %s {\n\n", model.getModelName()));
 
@@ -41,12 +48,18 @@ public class GeneratorController {
 
         classBuilder.append("}\n");
 
-        try (FileWriter writer = new FileWriter("/Users/hyun/Workspace/class_generator/generated_file/"+model.getModelName()+".java")) {
-            writer.write(classBuilder.toString());
-        } catch (IOException e) {
-            System.out.println("error: " + e.getMessage());
-        }
+//        try (FileWriter writer = new FileWriter("/Users/hyun/Workspace/class_generator/generated_file/"+model.getModelName()+".java")) {
+//            writer.write(classBuilder.toString());
+//        } catch (IOException e) {
+//            System.out.println("error: " + e.getMessage());
+//        }
 
-        return "";
+        byte[] classBytes = classBuilder.toString().getBytes();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + model.getModelName() + ".java");
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/octet-stream");
+
+        return new ResponseEntity<>(classBytes, headers, HttpStatus.OK);
     }
 }
